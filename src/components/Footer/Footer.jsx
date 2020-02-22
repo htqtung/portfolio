28ecf8099b8c627b6736576837332f18974d3801
Tung Huynh <htqtung@gmail.com>
2020-02-23 00:53:23 +0200

    edit content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,10 @@
 import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
-import GithubButtons from '../GithubButtons/GithubButtons';
-
-import { githubButtons } from '../../mock/data';
 
 const Footer = () => {
   const { footer } = useContext(PortfolioContext);
   const { networks } = footer;
-  const { isEnabled } = githubButtons;
 
   return (
     <footer className="footer navbar-static-bottom">
@@ -16,6 +12,7 @@ const Footer = () => {
         <a href="#top" aria-label="Back To Top" className="back-to-top">
           <i className="fa fa-angle-up fa-2x" aria-hidden="true" />
         </a>
+        {networks && <p>See my profiles at</p>}
         <div className="social-links">
           {networks &&
             networks.map(network => {
@@ -33,15 +30,6 @@ const Footer = () => {
               );
             })}
         </div>
-        <hr />
-        <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
-          </a>
-        </p>
-
-        {isEnabled && <GithubButtons />}
       </Container>
     </footer>
   );
