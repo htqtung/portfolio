5b830145e29e3feb4f2aded98f67f5904f0e9219
Tung Huynh <htqtung@gmail.com>
2020-02-27 21:56:50 +0200

    edit default info

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url || 'https://github.com/htqtung/portfolio'}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
